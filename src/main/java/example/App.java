baddb9072fac7ae1e87837bab9e71e8c4c6ac431
thashwini525 <123222816+thashwini525@users.noreply.github.com>
2024-10-18 13:06:51 +0530

    Update App.java

--- src/main/java/example/App.java
+++ src/main/java/example/App.java
@@ -5,6 +5,6 @@ package example;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("hii iam Thashwini");
     }
 }
